9b3ba1a8ee2f3af3d3ba9ec7d9ec29670e65d181
woojin <133613646+wo0odychan@users.noreply.github.com>
2023-08-02 15:23:29 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(function () {
+    //()함수, {}객체, []배열
+    $('.main_slide').slick({
+        arrows: false,
+        dots: true,
+        vertical: true,
+        autoplay: true,
+
+    });
+
+})
